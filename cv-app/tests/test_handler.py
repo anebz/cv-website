a4f63dd4a499a97f32ae713af3af228ce5284275
ane <anebzt@protonmail.com>
2022-03-08 17:02:09 +0100

    unified tests

--- cv-app/tests/test_handler.py
+++ cv-app/tests/test_handler.py
@@ -5,11 +5,17 @@ from unittest import mock
 
 from function import app
 
-with open('../../template.yaml', 'r') as f:
+with open('cv-app/template.yaml', 'r') as f:
     TABLENAME = re.search(r'TableName: (.*)?', f.read()).group(1)
 
 @mock.patch.dict(os.environ, {"TABLENAME": TABLENAME})
 def test_lambda_handler():
+
+    # check AWS credentials
+    print(os.environ.keys())
+    assert "AWS_ACCESS_KEY_ID" in os.environ
+    assert "AWS_SECRET_ACCESS_KEY" in os.environ
+
     ret = app.lambda_handler("", "")
 
     # assert return keys
